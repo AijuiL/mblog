d29647999858d668492ae15bc18d05999aab3c39
AijuiL <zx953159@gmail.com>
2017-02-16 18:41:14 +0800

    20160216-2

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,5 +1,6 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from datetime import  datetime
 from .models import Post
 
@@ -12,3 +13,13 @@ def homepage(request):
     html = template.render(locals())
 
     return  HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if Post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return  redirect('/')
