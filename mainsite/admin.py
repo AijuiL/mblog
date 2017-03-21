6edc05d8062a1ced7703988a7bb681e4ac4e1a34
AijuiL <zx953159@gmail.com>
2017-03-22 03:25:22 +0800

    new table for chinese leaguage

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import NewTable
 # Register your models here.
 
 
@@ -7,3 +8,4 @@ class PostAdmin(admin.ModelAdmin):
     list_display =  ('title', 'slug', 'pub_date')
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(NewTable)
--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.db import models
 from django.utils import timezone
 # Create your models here.
@@ -11,6 +12,24 @@ class Post(models.Model):
     class Meta:
         ordering = ('-pub_date',)
 
-
     def __unicode__(self):
         return self.title
+
+
+class NewTable(models.Model):
+    bigint_f = models.BigIntegerField(verbose_name=u"64位元之大整數")
+    bool_f   = models.BooleanField(verbose_name=u'布林值')
+    date_f   = models.DateField(auto_now=True,verbose_name=u'日期格式')
+    char_f   = models.CharField(max_length=20, unique=True,verbose_name=u'儲存資料字串Char')
+    datetime_f=models.DateTimeField(auto_now_add=True,verbose_name=u'資料時間格式')
+    decimal_f= models.DecimalField(max_digits=10, decimal_places=2,verbose_name=u'定點小數數值資料')
+    float_f  = models.FloatField(null=True,verbose_name=u"浮點數")
+    int_f    = models.IntegerField(default=2010,verbose_name=u'整數欄位')
+    text_f   = models.TextField(verbose_name=u'用在HTML表單')
+
+    class Meta:
+        verbose_name = u"新標題"
+        verbose_name_plural = u"新標題"
+
+
+
